e48a75d2032e171b6fa4906a66e9eee1b6dac0c0
Andrew Hollingworth <andrew.hollingworth@gmail.com>
2020-02-04 11:40:39 -0500

    Pre-interview practice. Worked on Coderpad.

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,25 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+// START PROGRAM
+//   Set an empty variable called largestSum
+//   ITERATE over the array from index 0 to inputArray's length - k
+//     ITERATE through the array, K times.
+//       Sum the entries in the array from i up to i+k.
+//     END ITERATE
+//     IF that currentSum > largestSum, then largestSum = currentSum.
+//     END IF
+//   END ITERATE
+//   RETURN largest Sum
+// END PROGRAM
 
+function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+  let largestSum = 0
+  for (let i = 0; i < (inputArray.length - k); i += 1) {
+    let currentSum = 0;
+    for (let j = 0; j < k; j += 1) {
+      currentSum += inputArray[i + j]
+      currentSum > largestSum ? largestSum = currentSum : '';
+    }
+  }
+  return largestSum
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
